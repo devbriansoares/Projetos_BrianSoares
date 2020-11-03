a66f4cc0c7d950fdfe27d11339ff09de55e7065d
Brian Soares <73753126+devbriansoares@users.noreply.github.com>
2020-11-03 10:22:49 -0300

    Python

--- Python/Desafios Gerais/Calculadora.py
+++ Python/Desafios Gerais/Calculadora.py
@@ -39,16 +39,16 @@ while True:
             print ("Valor de entrada inválido!")
         else:
             if operacao == "+":
-                print ("{}+{}= {}".format(num1,num2,soma(num1,num2)))
+                print ("{} + {}= {}".format(num1,num2,soma(num1,num2)))
                 break
             elif operacao == "-":
-                print ("{}-{}= {}".format(num1,num2,sub(num1,num2)))
+                print ("{} - {}= {}".format(num1,num2,sub(num1,num2)))
                 break
             elif operacao == "*":
-                print ("{}*{}= {}".format(num1,num2,mult(num1,num2)))
+                print ("{} * {}= {}".format(num1,num2,mult(num1,num2)))
                 break
             elif operacao == "/":
-                print ("{}/{}= {}".format(num1,num2,div(num1,num2)))
+                print ("{} / {}= {}".format(num1,num2,div(num1,num2)))
                 break
             else:
                 print ("----------ERRO NA OPERAÇÃO----------")
