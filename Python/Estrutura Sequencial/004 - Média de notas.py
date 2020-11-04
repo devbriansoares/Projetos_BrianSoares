7026d5e9f6c05c2bcd6e98fb54910aead6b49e2a
Brian Soares <briansoaresinfo@gmail.com>
2020-11-03 22:08:35 -0300

    Update 004 - Média de notas.py

--- Python/Estrutura Sequencial/004 - Média de notas.py
+++ Python/Estrutura Sequencial/004 - Média de notas.py
@@ -1,6 +1,6 @@
 #004 Faça um Programa que peça as
 #4 notas bimestrais e mostre a média.
-n1 = float(input("\nNota 1: "))
+n1 = float(input("Nota 1: "))
 n2 = float(input("Nota 2: "))
 n3 = float(input("Nota 3: "))
 n4 = float(input("Nota 4: "))
